6b3fe448b1a9332c54c19baf48be435894dfd727
daos12 <daos_ptc@hotmail.com>
2024-11-26 00:04:24 -0300

    posts

--- server.js
+++ server.js
@@ -1,6 +1,34 @@
 import express from "express";
 
+const posts = [
+    {
+      descricao: "Uma foto teste",
+      imagem: "https://placecats.com/millie/300/150" //Imagem aleatoria somente para teste
+    },
+    {
+      descricao: "Gato brincando com um novelo de lã",
+      imagem: "https://placecats.com/millie/300/150"
+    },
+    {
+      descricao: "Gatinho dormindo em uma caixa de papelão",
+      imagem: "https://placecats.com/millie/300/150"
+    },
+    {
+      descricao: "Gata olhando pela janela",
+      imagem: "https://placecats.com/millie/300/150"
+    },
+    {
+      descricao: "Grupo de gatinhos brincando",
+      imagem: "https://placecats.com/millie/300/150"
+    },
+    {
+      descricao: "Gato com um chapéu engraçado",
+      imagem: "https://placecats.com/millie/300/150"
+    }
+];
+
 const app = express();
+app.use(express.json()); //Serve para transitar textos en diversas linguagens
 
 //iniciar o servidor - 3000 = Porta padrão de acesso de servidor local
 app.listen(3000, () => {
@@ -8,12 +36,12 @@ app.listen(3000, () => {
 
 });
 
-//Demonstrar a rota/caminho definindo a resposta
-//Criando rota para cliente
-// req res = requisição e resposta
+//Demonstrar a rota/caminho definindo a resposta ↓
+//Criando rota para cliente ↓
+// req res = requisição e resposta ↓
 
-app.get("/api", (req,res) =>{
-    res.status(200).send("Rota Inicial"); //acessar navegador = localhost:3000/api
+app.get("/posts", (req,res) =>{
+    res.status(200).json(posts); //acessar navegador = localhost:3000/posts
 });
 
-
+//video 18 mim
